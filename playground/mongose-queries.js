d5557b3af620225d7fa960bd9e63524c586a8ebe
kriskh <klokkholst@gmail.com>
2018-09-12 22:57:45 +0200

    added mongoose queries

---
+++ playground/mongose-queries.js
@@ -0,0 +1,29 @@
+const {mongoose}=require('./../server/db/mongoose');
+const {Todo}=require('./../server/models/todo');
+const {ObjectID}=require('mongodb');
+
+var id='5b197672a77535242a124a79';
+ObjectID.isValid(id);
+
+
+Todo.find({
+    _id:id
+}).then((todos)=>{
+    console.log('Todos',todos);
+},(e)=>{
+    console.log('ID not found')
+});
+
+Todo.findOne({
+    _id:id
+}).then((todos)=>{
+    console.log('Todos',todos);
+},(e)=>{
+    console.log('ID not found')
+});
+
+Todo.findById(id).then((todos)=>{
+    console.log('Todos',todos);
+},(e)=>{
+    console.log('ID not found')
+});
